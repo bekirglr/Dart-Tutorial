e894be4e964fe9a13dc1b9698740cacc493e39ed
bekirglr <bbekirglr@gmail.com>
2024-04-30 15:24:33 +0300

    I learned variable types

---
+++ variable.dart
@@ -0,0 +1,31 @@
+void main() {
+//Banka var
+//Bankanın bir Bakiyesi olsun
+//Bankanın müşterileri var
+//BankaHer gelen yeni müşteriye +10 tl veriyor
+/*
+  const String bankName = "RC Bank";
+  double bankBalance = 100;
+  final String welcomeMessage = "Welcome";
+  final String customer1 = " Mr. Rico";
+  var customer1Balance = 50;
+  final String customer2 = " Mrs. Jennie";
+  var customer2Balance = 70;
+
+  print(bankName + " " + '$bankBalance');
+  print(welcomeMessage + customer1 + ": " + '$customer1Balance');
+  print(welcomeMessage + customer2 + ": " + '$customer2Balance');
+
+  print("-------------------");
+
+  print(customer1 + ": " + '${customer1Balance + 10}');
+  print(customer2 + ": " + '${customer2Balance + 10}');
+  */
+
+  double sayi1 = 20.5;
+  int sayi2 = 30;
+
+  int sum = (sayi1 + sayi2).toInt(); //var'da kullanılabilir
+
+  print(sum);
+}
