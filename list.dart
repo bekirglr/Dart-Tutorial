540ffe796080cc3babb4d146898b348a14f99854
bekirglr <bbekirglr@gmail.com>
2024-05-03 19:53:27 +0300

    for loop learned

--- list.dart
+++ list.dart
@@ -12,7 +12,7 @@ void main() {
   } else {
     print("Yetersiz Para!");
   }
-/* ---------------------------------------- */
+
   //Bankada ki müşterilerin parası var
   //Parası azdan çoğa göre sırala
   //Bankaya yeni müşteri parası ekle
@@ -27,4 +27,36 @@ void main() {
   print(moneys);
   moneys.insert(3, 400);
   print(moneys);
+
+/* --------------LOOP--------------------- */
+  print('**************************');
+//Bankada her müşterinin belirli miktarda parası olsun
+//Parası 100'den büyük olan kredisini kapatsın
+//Parası 45'ten büyük olan borcunu ödesin
+//Parası 45'ten küçük olan ödeyemesin
+
+  List<int> customerMoneys = [35, 60, 100, 45];
+  customerMoneys.sort();
+  for (int i = 0; i < customerMoneys.length; i++) {
+    print("Customer${i} Money: ${customerMoneys[i]}");
+    if (customerMoneys[i] >= 100) {
+      print('Krediniz Kapandı!');
+    } else if (customerMoneys[i] >= 45) {
+      print('Asgari borç ödendi!');
+    } else {
+      print('Borcunuzu Ödeyiniz!');
+    }
+  }
+  //Yapılan işlemlerin birde tersini yap
+  print('-----Ters--------');
+  for (int i = customerMoneys.length - 1; i >= 0; i--) {
+    print("Customer${i} Money: ${customerMoneys[i]}");
+    if (customerMoneys[i] >= 100) {
+      print('Krediniz Kapandı!');
+    } else if (customerMoneys[i] >= 45) {
+      print('Asgari borç ödendi!');
+    } else {
+      print('Borcunuzu Ödeyiniz!');
+    }
+  }
 }
