2c5cc558490bbe1a571c0df55b8c4cab03f44eed
bekirglr <bbekirglr@gmail.com>
2024-05-04 22:21:38 +0300

    exp 2

--- AlgorithmPractices/algorithms.dart
+++ AlgorithmPractices/algorithms.dart
@@ -7,17 +7,25 @@
 */
 
 void main() {
-//********1********
-  final List<int> numbers = [2, 8, 5, 0, 99];
-  int max = numbers[
-      0]; // Başlangıçta en büyük sayıyı ilk eleman olarak kabul ediyoruz
-  for (var i = 0; i < numbers.length; i++) {
-    // Eğer mevcut eleman, şu ana kadar bulduğumuz en büyük sayıdan büyükse
-
-    if (numbers[i] > max) {
-      // Eğer mevcut eleman, şu ana kadar bulduğumuz en büyük sayıdan büyükse
-      max = numbers[i]; // Bu elemanı en büyük olarak güncelliyoruz
+  print('***********EXP 1***********');
+  final List<int> expList1 = [2, 8, 5, 0, 99];
+  int max = expList1[0];
+  for (var i = 0; i < expList1.length; i++) {
+    if (expList1[i] > max) {
+      max = expList1[i]; // Bu elemanı en büyük olarak güncelliyoruz
     }
   }
   print('Dizinin en büyük sayısı: $max');
+
+  print('***********EXP 2***********');
+  final List<int> expList2 = [1, 5, 0, 99];
+  var expSum2 = 0;
+
+  for (var i = 0; i < expList2.length; i++) {
+    expSum2 += expList2[i];
+  }
+  // for (var i in expList2) {
+  //   expSum2 += i;
+  // }
+  print(expSum2);
 }
