de6b14526e2d80964e0c8164f151cc5e5bf71209
bekirglr <bbekirglr@gmail.com>
2024-05-04 18:50:08 +0300

    Learned method and exp

--- methods.dart
+++ methods.dart
@@ -1,14 +1,21 @@
 void main() {
-  //Müşterinin parası var mı yok mu kontrol et.
-
   checkUserMoney(0, 50);
   checkUserMoney(20, 50);
   checkUserMoney(200, 50);
   print('*********************');
   checkPrimeNumber(5);
   checkPrimeNumber(9);
+  print('*********************');
+  final int result = convertToDolar(100, 33);
+  print('total dolar: ${result}');
+  final int resultEuro = convertToEuro(turkisLiras: 100, euro: 14);
+  print('total euro: ${resultEuro}');
+  print('*********************');
+  final String resultMessage = sayHello("Rico");
+  print(resultMessage);
 }
 
+//Check user money funciton
 void checkUserMoney(int money, int minimumValue) {
   if (money > minimumValue) {
     print('Paran Var');
@@ -17,7 +24,7 @@ void checkUserMoney(int money, int minimumValue) {
   }
 }
 
-//Check prime-number
+//Check prime-number funciton
 void checkPrimeNumber(int number) {
   bool isPrime = true;
   for (int j = 2; j <= number / 2; j++) {
@@ -31,4 +38,18 @@ void checkPrimeNumber(int number) {
   } else {
     print('$number is Not a Prime Number!');
   }
+}
+
+//Convert turkish liras to dolar funciton
+int convertToDolar(int tliras, int dolar) {
+  return tliras ~/ dolar;
+}
+
+int convertToEuro({required turkisLiras, int euro = 14}) {
+  return turkisLiras ~/ euro;
+}
+
+//Say hello funciton
+String sayHello(String name) {
+  return 'Hello $name!';
 }
