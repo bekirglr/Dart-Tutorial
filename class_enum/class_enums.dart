83b4d51c2e2a1e7cf0334abe88197a4222ab5fb3
bekirglr <bbekirglr@gmail.com>
2024-05-06 17:29:41 +0300

    enums

---
+++ class_enum/class_enums.dart
@@ -0,0 +1,25 @@
+void main() {
+  int userMoney = 100;
+  final customerBuy = Product.oil;
+  switch (customerBuy) {
+    case Product.bread:
+      userMoney -= 5;
+      print(userMoney);
+    case Product.oil:
+      userMoney -= 50;
+      print(userMoney);
+    case Product.milk:
+      userMoney -= 10;
+      print(userMoney);
+    case Product.water:
+      userMoney -= 5;
+      print(userMoney);
+  }
+}
+
+enum Product {
+  bread,
+  oil,
+  milk,
+  water,
+}
