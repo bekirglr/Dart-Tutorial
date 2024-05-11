84d1b49968a6f327e9b677e5bc57dc56b839e683
bekirglr <bbekirglr@gmail.com>
2024-05-11 21:03:48 +0300

    is and as

--- class_advance/main.dart
+++ class_advance/main.dart
@@ -1,18 +1,28 @@
 void main() {
-  final user1 = _User("Rico", age: 19);
+  final user1 = _User("Rico", age: 22);
 
   // if ((user1.age ?? 0) < 18) {}
-  if (user1.age is int) {
+  if (user1.age is! int) {
     if (user1.age! < 18) {
       print("18'den küçük!!");
       user1.updateMoneyWithString("15");
+    } else {
+      user1.updateMoneyWithNumber(22);
     }
   }
-  // "!" ile force kullanmamda sakınca yok
-  //çünkü birinci if içerisinde age'in null olmadığını
-  // int mi? sorusu ile sağlamış oldum. Eğer int ise ... yani null değil ise ...
-  //Kullancının parası var ama paranın tipi değişkenlik gösterebiliyor (String int)
-  //Parası olan kullanıcının verisi "on beş"'.de yazabilir "15"'te yazabilir
+
+  final _newType = user1.moneyType is String ? user1.moneyType is String : "";
+  //moneyType string mi? değilse "" bunun ver
+
+  print("x $_newType");
+
+  /* "!" ile force kullanmamda sakınca yok
+  çünkü birinci if içerisinde age'in null olmadığını
+  int mi? sorusu ile sağlamış oldum. Eğer int ise ... yani null değil ise ...
+  */ /*
+  Kullancının parası var ama paranın tipi değişkenlik gösterebiliyor (String int)
+  Parası olan kullanıcının verisi "on beş"'.de yazabilir "15"'te yazabilir
+  */
 }
 
 class _User {
@@ -34,6 +44,6 @@ class _User {
 /*
 dynamic değişkenlerde ilk olarak hangi tanım kullanılırsa kod o tanımla çalışmaya devam eder ve
 ikinci tanım patlar.
-Örneğin yukarıda ki kod parçasında moneyType hem String hem de int olarak tanımlanmış. 
-ana sınıf içerisnde ise String olarak kullanılmış. O halde artık moneyType dynamic'lik özelliğini kaybedip
+Yani, bir değişkenin türü tanımlandığında kesin olarak belirtilmez,
+bunun yerine programın çalışma zamanında belirlenir.
 */
