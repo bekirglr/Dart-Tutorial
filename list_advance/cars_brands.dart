6ee3e76826bedb3aedba21c5276e140624f3bbe6
bekirglr <bbekirglr@gmail.com>
2024-05-12 22:54:59 +0300

    any-where-contains

---
+++ list_advance/cars_brands.dart
@@ -0,0 +1,42 @@
+class CarBrand {
+  final carBrands brand;
+  final int model;
+  final double price;
+  final String? carType;
+  late bool isSecondHand;
+
+  CarBrand({
+    required this.brand,
+    required this.model,
+    required this.price,
+    this.carType,
+    this.isSecondHand = true,
+  });
+
+  @override
+  bool operator ==(covariant CarBrand other) {
+    if (identical(this, other)) return true;
+
+    return other.brand == brand &&
+        other.model == model &&
+        other.price == price &&
+        other.carType == carType &&
+        other.isSecondHand == isSecondHand;
+  }
+
+  @override
+  int get hashCode {
+    return brand.hashCode ^
+        model.hashCode ^
+        price.hashCode ^
+        carType.hashCode ^
+        isSecondHand.hashCode;
+  }
+}
+
+enum carBrands {
+  BMW,
+  Mercedes,
+  Audi,
+  Volvo,
+}
---
+++ list_advance/list_advance.dart
@@ -0,0 +1,56 @@
+import 'cars_brands.dart';
+
+void main() {
+  var line = "*" * 10;
+  /*
+  Arabalar listesi oluştur.
+  Arabanın markası(brand), modeli, ücreti kesin olacak.
+  Arabanın tipi olmayabilir.
+  Kullanıcı belirtmediği sürece tüm araçlar ikinci el sayılacak.
+  */
+
+  final carBrand = CarBrand(
+      brand: carBrands.Audi, model: 2020, price: 300.000, isSecondHand: false);
+  print(carBrand);
+
+  //4 tanem arabam var
+  final carItems = [
+    CarBrand(
+      brand: carBrands.BMW,
+      model: 2024,
+      price: 270.000,
+    ),
+    CarBrand(
+      brand: carBrands.Audi,
+      model: 2013,
+      price: 300.000,
+    ),
+    CarBrand(
+        brand: carBrands.Mercedes,
+        model: 1980,
+        price: 350.000,
+        isSecondHand: false),
+    CarBrand(
+      brand: carBrands.Volvo,
+      model: 2021,
+      price: 250.000,
+    ),
+  ];
+
+  final newCar = CarBrand(brand: carBrands.Volvo, model: 2021, price: 250.000);
+  print(line);
+
+  //Kaç tanesi ikinci el?
+  final resultCount =
+      carItems.where((element) => element.isSecondHand = true).length;
+  print(resultCount);
+  print(line);
+
+  //Yeni bir araba geldi. Bizde var gibi?
+  final isHaveCar = carItems.contains(newCar);
+  if (isHaveCar) {
+    print("Elimizde Aynısı var!");
+  } else {
+    print("Elimizde yok!");
+  }
+}
