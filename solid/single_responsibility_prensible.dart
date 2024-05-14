d03bebadfaac53607141e4c8cf9a976f4c38c179
bekirglr <bbekirglr@gmail.com>
2024-05-15 01:23:00 +0300

    srp

---
+++ solid/single_responsibility_prensible.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+void main() {}
+
+//Bir sınıfın yalnızca bir nedeni olmalıdır
+
+//Bu sınıf kullanıcı yönetim sınıfıdır. Başka bir işlem oluşturmak bu prensibe uymaz
+class UserManager {
+  String name;
+
+  UserManager({required this.name});
+
+  void updateUserName(String name) {
+    this.name = name;
+  }
+}
+
+//Bu sınıf user'ın local ve name'ini update ediyor.
+class UserLocalization {
+  final UserManager manager;
+
+  UserLocalization(this.manager);
+
+  void updateNameAndLoclaziaitopn() {
+    manager.updateUserName('a');
+    changeLocalizaiton();
+  }
+
+  void changeLocalizaiton() {
+    print('object');
+  }
+}
