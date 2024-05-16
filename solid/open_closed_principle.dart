f4f866ecf41bf5681b3654465a812e1d2d9bf00f
bekirglr <bbekirglr@gmail.com>
2024-05-16 23:40:54 +0300

    OCP

--- solid/open_closed_principle.dart
+++ solid/open_closed_principle.dart
@@ -15,5 +15,4 @@ class ProductCategories extends Product {
 Yazılım varlıkları (sınıflar, modüller, fonksiyonlar vb.), genişletmeye açık ancak değişime kapalı olmalıdır. 
 Yani mevcut kod değişmeden yeni davranışlar eklenmeli veya mevcut davranışlar değiştirilmelidir. 
 Bu prensip, kodun esnekliğini ve uzun vadeli bakımını kolaylaştırır.
-:readme.md
 */
